ac62bb57f974ebdc93806f3e74158cf0e5019342
Camila-Ribeiro <camilarf@gmail.com>
2020-06-24 12:47:00 +0000

    deleted node_modules and package-lock.json, crea, created import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -6,7 +7,7 @@ from bson.objectid import ObjectId
 if os.path.exists('env.py'):
     import env
 
-# ENVIROMENT VARIABLES
+""" ENVIROMENT VARIABLES """
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = os.environ.get('SECRET_KEY')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
@@ -14,13 +15,21 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
 
-@app.route('/')
-# def root():
-#     return render_template('index.html')
+api_key = "7b98ae9af6fe4cc6b9a33b00e08db54d"
 
-@app.route('/get_home')
-def get_home():
-    return render_template('index.html')
+
+@app.route('/', methods=['GET'])
+def index():
+    r = requests.get('https://api.spoonacular.com/recipes/random?number=10&apiKey='+api_key+'')
+    json_obj = r.json() 
+    recipes = json_obj['recipes']['title']
+    for x in json_obj:
+        print (x)
+    return render_template('index.html', random = recipes)
+
+# @app.route('/get_home')
+# def get_home():
+#     return render_template('index.html')
     # return render_template('index.html', tasks=mongo.db.tasks.find())
 
 @app.route('/get_allRecipes')
