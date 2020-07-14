a5a37901587fe665022462fb8abedd8bc83c434d
Camila <camilarf@gmail.com>
2020-07-14 20:37:49 +0100

    indentation loine 259

--- app.py
+++ app.py
@@ -256,5 +256,5 @@ def get_meals():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT', 27017)),
-    debug=True)
+            port=int(os.environ.get('PORT', 27017)),
+            debug=True)
